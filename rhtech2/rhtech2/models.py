082c7a246d10f61eb6713fd74b67d4c99d592ea8
leohfigueiredo <leohfigueiredo@gmail.com>
2022-05-22 15:28:21 +0100

    Commit show_names

--- rhtech2/rhtech2/models.py
+++ rhtech2/rhtech2/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Departamento(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
 
 
 class Colaborador(models.Model):
